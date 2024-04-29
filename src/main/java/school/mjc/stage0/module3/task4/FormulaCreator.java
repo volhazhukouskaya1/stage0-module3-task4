54cf95d67a4ed5e8c24213a60f489d3c509c05dc
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-04-29 16:51:31 +0300

    task 4

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-    }
+        double result;
+        double a =1;
+        double b=2;
+        result = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        System.out.println(result);
+           }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+    char charC = 99;
+    char charH = 104;
+    char charA = 97;
+    char charR = 114;
+    System.out.print(charC);
+    System.out.print(charH);
+    System.out.print(charA);
+    System.out.print(charR);
     }
 }
